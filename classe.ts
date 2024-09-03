cc732ad40278f43476b27ba76276b58e71a5bbb3
Angelo Luz <angelogl@gmail.com>
2024-09-03 09:58:59 -0300

    Update text to Portuguese

--- classe.ts
+++ classe.ts
@@ -4,7 +4,7 @@ import { Util } from "./util";
 export class Guerreiro extends Personagem {
     constructor(nome: string) {
         super(
-            nome + " Warrior",
+            nome + " Guerreiro",
             Util.randomizar(1,1000),
             0,
             0,
